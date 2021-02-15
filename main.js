ccbbaafddc6f8cceddec646b5a02e874a5bf7840
agrah355 <74407072+agrah355@users.noreply.github.com>
2021-02-15 21:11:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var user_name;
-function add_user(){
+
+function login(){
     user_name=document.getElementById("user_name").value;
-    localStorage.setItem("user name",user_name);
+    localStorage.setItem("name", user_name);
     window.location="second_page.html";
 }
